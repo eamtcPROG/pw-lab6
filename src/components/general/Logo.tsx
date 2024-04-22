1582ba7897e94954d211cedac739b7d0480a96f0
Coretchi Mihai <prog.eamtc@gmail.com>
2024-04-22 10:04:42 +0300

    Top bar

---
+++ src/components/general/Logo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import logo from "assets/images/logo.png";
+
+const Logo:React.FC = () => {
+    return (<img src={logo} alt="logo" width={50} height={50}/>)
+}
+
+export { Logo };
